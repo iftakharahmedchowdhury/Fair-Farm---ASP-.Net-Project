b0e3ffdaa72e1250e91c979a68884006e5789d81
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2023-12-26 07:53:32 +0600

    Making DTOS

---
+++ BLL/DTOs/StoredItemInColdStorageDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Models
+{
+    public class StoredItemInColdStorage
+    {
+    
+        public int Id { get; set; }
+      
+        public string CropsName { get; set; }
+      
+        public decimal CropsQuantity { get; set; }
+    
+        public int Duration { get; set; }
+       
+        public string Region { get; set; }
+
+      
+        public decimal TotalRent { get; set; }
+
+      
+        public int RequestId { get; set; }
+     
+        public int ColdStorageId { get; set; }
+      
+    }
+}
