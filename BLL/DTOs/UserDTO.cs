01ab2055c157382fe7737d3e9d9f465a91bb56ff
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2023-12-27 20:33:47 +0600

    add

--- BLL/DTOs/UserDTO.cs
+++ BLL/DTOs/UserDTO.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace BLL.DTOs
 {
@@ -13,7 +15,7 @@ namespace BLL.DTOs
         public string UserName { get; set; }
   
         public string UsersUserName { get; set; }
-   
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must contain at least one uppercase letter, one number, and be at least 8 characters long.")]
         public string Password { get; set; }
   
         public string UserPhoneNumber { get; set; }
@@ -23,7 +25,7 @@ namespace BLL.DTOs
         public string UserCity { get; set; }
   
         public string UserRegion { get; set; }
-   
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$", ErrorMessage = "Invalid email address.")]
         public string UserEmail { get; set; }
 
         public bool UserRedList { get; set; }
