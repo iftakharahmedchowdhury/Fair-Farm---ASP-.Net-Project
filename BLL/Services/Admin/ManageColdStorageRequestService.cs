efb0782847a916148c59ded3654d278f48b2376f
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2023-12-30 19:32:03 +0600

    manage stored item in cold storage implemented

--- BLL/Services/Admin/ManageColdStorageRequestService.cs
+++ BLL/Services/Admin/ManageColdStorageRequestService.cs
@@ -14,7 +14,11 @@ namespace BLL.Services.Admin
     {
         public static List<RequestTableDTO> GetColdStorageRequest()
         {
-            var requestData = DataAccessFactory.RequestCropData().Get();
+           /*var requestData = DataAccessFactory.RequestCropData().Get();*/
+            var requestData = DataAccessFactory.RequestCropData().Get()
+                            .Where(item => item.RequestType == "ColdStorage")
+                            .ToList();
+
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<RequestTable, RequestTableDTO>();
@@ -23,17 +27,22 @@ namespace BLL.Services.Admin
             var mapper = new Mapper(config);
 
             var requestList = mapper.Map<List<RequestTableDTO>>(requestData);
+            
 
             foreach (var request in requestList)
             {
-                var requestItems = new List<ColdStorageItemListDTO>();
+                
+                    var requestItems = new List<ColdStorageItemListDTO>();
 
-                var requestItem = DataAccessFactory.ColdStorageRequestItemData().GetItem(request.Id);
+                    var requestItem = DataAccessFactory.ColdStorageRequestItemData().GetItem(request.Id);
 
-                var data2 = mapper.Map<List<ColdStorageItemListDTO>>(requestItem);
-                requestItems.AddRange(data2);
+                    var data2 = mapper.Map<List<ColdStorageItemListDTO>>(requestItem);
 
-                request.ColdStorageRequestItems = requestItems;
+                    requestItems.AddRange(data2);
+
+                    request.ColdStorageRequestItems = requestItems;
+                
+               
             }
 
             return requestList;
@@ -87,6 +96,52 @@ namespace BLL.Services.Admin
                 request.Status = "NotAccepted";
                 DataAccessFactory.RequestCropData().Update(request);
             }
+        }
+
+
+
+
+        public static List<StoredItemInColdStorageDTO> GetAllItem()
+        {
+            var data = DataAccessFactory.ColdStorageStoreData().Get();
+          
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<StoredItemInColdStorage, StoredItemInColdStorageDTO>();
+            });
+            var mapper = new Mapper(config);
+            return mapper.Map<List<StoredItemInColdStorageDTO>>(data);
+        }
+
+        public static StoredItemInColdStorageDTO Get(int id)
+        {
+            var data = DataAccessFactory.ColdStorageStoreData().Get(id);
+
+            if (data != null)
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<StoredItemInColdStorage, StoredItemInColdStorageDTO>();
+                });
+                var mapper = new Mapper(config);
+                return mapper.Map<StoredItemInColdStorageDTO>(data);
+            }
+
+            return null;
+        }
+
+        public static bool Delete(int id)
+        {
+            var coldStrItemData = DataAccessFactory.ColdStorageStoreData().Get(id);
+
+            if (coldStrItemData != null)
+            {
+                var isDeleted = DataAccessFactory.ColdStorageStoreData().Delete(id);
+
+                return isDeleted;
+            }
+
+            return false;
         }
     }
 }
--- Controllers/Admin/ManageColdStorageRequestController.cs
+++ Controllers/Admin/ManageColdStorageRequestController.cs
@@ -99,6 +99,76 @@ namespace Fair_Farm.Controllers.Admin
         }
 
 
+        //admin can see all item from cold storage
+        [AdminAccess]
+
+        [HttpGet]
+        [Route("api/ColdStorageItem/all")]
+        public HttpResponseMessage AllItemFromColdStorage()
+        {
+            try
+            {
+                var data = ManageColdStorageRequestService.GetAllItem();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+
+            }
+        }
+
+
+        //admin can see specific item in cold storage
+        [AdminAccess]
+        [HttpGet]
+        [Route("api/ColdStorageItem/{id}")]
+        public HttpResponseMessage GetColdStorageItemById(int id)
+        {
+            try
+            {
+                var data = ManageColdStorageRequestService.Get(id);
+
+                if (data != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, new { Message = "Item not found" });
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+
+        // admin can delete specific item from cold storage
+        [AdminAccess]
+        [HttpDelete]
+        [Route("api/ColdStorageItem/delete/{id}")]
+        public HttpResponseMessage DeleteColdStorageItem(int id)
+        {
+            try
+            {
+                var isDeleted = ManageColdStorageRequestService.Delete(id);
+                if (isDeleted)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Item deleted successfully" });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, new { Message = "Item not found" });
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+
 
     }
 }
